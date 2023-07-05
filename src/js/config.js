51f855afde735428e750a65700d6bc9aa7454cf9
Tom Burzynski <tellur.beryl@gmail.com>
2023-07-05 12:46:48 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/"; // USE CAPITAL LETTERS AS THIS IS A CONSTANT THAT WILL NEVER CHANGE
+
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
---
+++ src/js/controller.js
@@ -0,0 +1,84 @@
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
+import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
+import paginationView from "./views/paginationView.js";
+
+
+import 'core-js/stable'; // Polyfilling
+import 'regenerator-runtime/runtime'; // Polyfilling
+import {
+  async
+} from 'regenerator-runtime';
+
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+const controlRecipes = async function () {
+  try {
+
+    const id = window.location.hash.slice(1);
+
+    if (!id) return; // guard clause
+    recipeView.renderSpiner();
+
+    // 1) Loading recipe
+    await model.loadRecipe(id); // Async function, so it's going to return a promise - We have to await this promise before we move on
+
+    // 2) Rendering the recipe
+    recipeView.render(model.state.recipe);
+
+  } catch (err) {
+    recipeView.renderError();
+  }
+}
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpiner();
+    console.log(resultsView);
+
+    //1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //2) Load search results
+    await model.loadSearchResults(query);
+
+    //3) Render results
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultPage());
+
+    //4) render initial pagination buttons
+    paginationView.render(model.state.search)
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  //1) Render new results
+  // resultsView.render(model.state.search.results);
+  resultsView.render(model.getSearchResultPage(goToPage));
+
+  //2) render NEW pagination buttons
+  paginationView.render(model.state.search)
+}
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+}
+
+init();
